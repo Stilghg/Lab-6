d29b587054a4d6eda31b932f24008e2db323f4ee
stilghg <tatibaym5@gmail.com>
2025-10-22 22:11:39 +0500

    1 комит(хз че это)

---
+++ 601.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+int main() {
+    char n;
+    cin >> n;
+    n=toupper(n);
+    cout << n;
+    
+    return 0;
+}
